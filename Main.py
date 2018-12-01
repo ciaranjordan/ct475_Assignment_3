2a6e0c5e59620fbb9772065cf2478259abed140e
Ciaran <c.jordan9@nuigalway.ie>
2018-12-01 18:26:46 +0000

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,222 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+import pandas as pd
+
+style.use('ggplot')
+
+data_file_name = 'owls_test.csv'
+index_of_class = 4
+
+
+class SupportVectorMachine:
+    def __init__(self, visualization=False):
+        self.visualization = visualization
+        self.colors = {1: 'r', -1: 'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1, 1, 1)
+
+    # train
+    def fit(self, data):
+        self.data = data
+        # { ||w||: [w,b] }
+        opt_dict = {}
+
+        num_samples, num_features = data.shape()
+
+        transforms = self.generateTransforms(num_features)
+
+        all_data = []
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+
+        print(self.data)
+
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        # support vectors yi(xi.w+b) = 1
+
+        step_sizes = [self.max_feature_value * 0.1,
+                      self.max_feature_value * 0.01,
+                      # point of expense:
+                      self.max_feature_value * 0.001,
+                      ]
+
+        # extremely expensive
+        b_range_multiple = 2
+        # we dont need to take as small of steps
+        # with b as we do w
+        b_multiple = 5
+        latest_optimum = self.max_feature_value * 10
+
+        for step in step_sizes:
+            w = np.array([latest_optimum, latest_optimum, latest_optimum, latest_optimum])
+            # we can do this because convex
+            optimized = False
+            while not optimized:
+                for b in np.arange(-1 * (self.max_feature_value * b_range_multiple),
+                                   self.max_feature_value * b_range_multiple,
+                                   step * b_multiple):
+                    for transformation in transforms:
+                        w_t = w * transformation
+                        found_option = True
+                        # weakest link in the SVM fundamentally
+                        # SMO attempts to fix this a bit
+                        # yi(xi.w+b) >= 1
+                        #
+                        # #### add a break here later..
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi * (np.dot(w_t, xi) + b) >= 1:
+                                    found_option = False
+                                    # print(xi,':',yi*(np.dot(w_t,xi)+b))
+
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+
+                if w[0] < 0:
+                    optimized = True
+                    print('Optimized a step.')
+                else:
+                    w = w - step
+
+            norms = sorted([n for n in opt_dict])
+            # ||w|| : [w,b]
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0] + step * 2
+
+        for i in self.data:
+            for xi in self.data[i]:
+                yi = i
+                print(xi, ':', yi * (np.dot(self.w, xi) + self.b))
+
+    def generateTransforms(self, num_features):
+        # Todo generate transform matrix
+
+        transforms = [[1, 1, 1, 1],
+                      [-1, 1, 1, 1],
+                      [1, -1, 1, 1],
+                      [1, 1, -1, 1],
+                      [1, 1, 1, -1],
+                      [-1, -1, 1, 1],
+                      [-1, 1, -1, 1],
+                      [-1, 1, 1, -1],
+                      [1, -1, -1, 1],
+                      [1, -1, 1, -1],
+                      [1, 1, -1, -1],
+                      [-1, -1, -1, 1],
+                      [-1, -1, 1, -1],
+                      [-1, 1, -1, -1],
+                      [1, -1, -1, -1],
+                      [-1, -1, -1, -1]]
+
+        return transforms
+
+    def predict(self, features):
+        # sign( x.w+b )
+        classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
+        return classification
+
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane = x.w+b
+        # v = x.w+b
+        # psv = 1
+        # nsv = -1
+        # dec = 0
+        def hyperplane(x, w, b, v):
+            return (-w[0] * x - b + v) / w[1]
+
+        datarange = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k')
+
+        # (w.x+b) = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], 'k')
+
+        # (w.x+b) = 0
+        # positive support vector hyperplane
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
+
+        plt.show()
+
+
+data_dict = {-1: np.array([[1, 7],
+                           [2, 8],
+                           [3, 8], ]),
+
+             1: np.array([[5, 1],
+                          [6, -1],
+                          [7, 3], ])}
+
+data = pd.read_csv(data_file_name)
+
+data = data.values
+np.random.shuffle(data)
+
+train_data_len = int((2 * len(data)) / 3)
+
+train_data = data[:train_data_len]
+test_data = data[train_data_len:]
+
+# print(train_data)
+
+# Now I want to splilt the data up into two classes, -1 and 1
+X_train = train_data[:, :-1]
+y_train = train_data[:, -1]
+
+class_1neg = [[]]
+class_1pos = [[]]
+
+for i in range(0, len(X_train)):
+    if y_train[i] == "LongEaredOwl":
+        class_1neg.append(X_train[i])
+    elif y_train[i] == "SnowyOwl":
+        class_1pos.append(X_train[i])
+
+class_1neg.pop(0)
+class_1pos.pop(0)
+
+np.asarray(class_1neg)
+np.asarray(class_1pos)
+
+data_dict = {-1: class_1neg,
+
+             1: class_1pos}
+
+print(data_dict)
+
+svm = SupportVectorMachine()
+svm.fit(data=data_dict)
+
+X_test = test_data[:, :-1]
+y_test = test_data[:, -1]
+
+for i in range(0, len(X_test)):
+    print("Test ", i)
+    print("Predicted: ", svm.predict(X_test[i]))
+    print("Actual: ", y_test[i], "\n")
+
+#svm.visualize()
